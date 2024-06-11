f5c3ec17f4aba715e934334c04726ee709788114
M Haidar Hanif <git@mhaidarhanif.com>
2024-06-11 22:30:03 +0700

    feat: seed products data

---
+++ prisma/data/products.ts
@@ -0,0 +1,20 @@
+export const dataProducts = [
+  {
+    slug: "head-panda-cushion", // unique
+    name: "Head Panda Cushion", // unique
+    imageURL:
+      "https://safariwonders.com/wp-content/uploads/2023/06/h11xxx003305-head-panda-cushion-1686803573.jpg",
+    price: 115000,
+    description: "Tempat perangkat/tas mini kepala Panda",
+    sku: "H11XXX003305", // unique
+  },
+  {
+    slug: "big-head-cuddly-elephant",
+    name: "Big Head Cuddly Elephant",
+    imageURL:
+      "https://safariwonders.com/wp-content/uploads/2023/06/t01xxx000003-big-head-cuddly-elephant-1686803856.jpg",
+    price: 105000,
+    description: "Mainan Boneka Gajah Mini",
+    sku: "T01XXX000003",
+  },
+];
