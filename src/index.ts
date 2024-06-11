370d42ceb7c1af96a3869046d8aef0a40d6b0572
M Haidar Hanif <git@mhaidarhanif.com>
2024-06-11 22:33:07 +0700

    feat: get all products endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { prisma } from "./libs/db";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.json({
+    message: "Amazing Safari Backend API",
+    products: "/products",
+  });
+});
 
-export default app
+app.get("/products", async (c) => {
+  const products = await prisma.product.findMany();
+
+  return c.json(products);
+});
+
+export default app;
