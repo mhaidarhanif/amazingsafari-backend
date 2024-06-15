cc35ad4ef949be5c5fde63538907acc531e80a9c
M Haidar Hanif <git@mhaidarhanif.com>
2024-06-15 16:07:22 +0700

    fix: setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+
 import { prisma } from "./libs/db";
 
 const app = new Hono();
+
+app.use("/*", cors());
 
 app.get("/", (c) => {
   return c.json({
