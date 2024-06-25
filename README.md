bf05604a9d8e56df2b6cd091b37dcfdbdf636e92
M Haidar Hanif <git@mhaidarhanif.com>
2024-06-25 22:36:14 +0700

    feat: auth register, with hash password

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # Amazing Safari Backend
 
+## REST API Specification
+
+- Production: `https://amazingsafari-backend.haidar.dev`
+- Local: `http://localhost:3000`
+
+Products:
+
+| Endpoint        | HTTP     | Description          |
+| --------------- | -------- | -------------------- |
+| `/products`     | `GET`    | Get all products     |
+| `/products/:id` | `GET`    | Get product by id    |
+| `/products`     | `POST`   | Add new product      |
+| `/products`     | `DELETE` | Delete all products  |
+| `/products/:id` | `DELETE` | Delete product by id |
+| `/products/:id` | `PUT`    | Update product by id |
+
+Auth:
+
+| Endpoint           | HTTP     | Permission    |
+| ------------------ | -------- | ------------- |
+| `/users`           | `GET`    | Public        |
+| `/users/:username` | `GET`    | Public        |
+| `/auth/register`   | `POST`   | Public        |
+| `/auth/login`      | `POST`   | Public        |
+| `/auth/me`         | `GET`    | Authenticated |
+| `/auth/logout`     | `POST`   | Authenticated |
+| `/cart`            | `GET`    | Authenticated |
+| `/cart/items`      | `POST`   | Authenticated |
+| `/cart/items/:id`  | `DELETE` | Authenticated |
+| `/cart/items/:id`  | `PUT`    | Authenticated |
+
 ## Getting Started
 
 To install dependencies:
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { zValidator } from "@hono/zod-validator";
 
 import { prisma } from "./libs/db";
+import { z } from "zod";
+import { hashPassword } from "./libs/password";
 
 const app = new Hono();
 
@@ -18,6 +21,101 @@ app.get("/products", async (c) => {
   const products = await prisma.product.findMany();
 
   return c.json(products);
+});
+
+app.get("/users", async (c) => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      username: true,
+    },
+  });
+
+  return c.json(users);
+});
+
+app.get("/users/:username", async (c) => {
+  const username = c.req.param("username");
+
+  const user = await prisma.user.findUnique({
+    where: { username },
+    select: {
+      id: true,
+      username: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  if (!user) {
+    c.status(404);
+    c.json({ message: "User not found" });
+  }
+
+  return c.json(user);
+});
+
+app.post(
+  "/auth/register",
+  zValidator(
+    "json",
+    z.object({
+      username: z.string(),
+      email: z.string(),
+      password: z.string(),
+    })
+  ),
+  async (c) => {
+    const body = c.req.valid("json");
+
+    try {
+      const newUser = await prisma.user.create({
+        data: {
+          username: body.username,
+          email: body.email,
+          password: {
+            create: {
+              hash: await hashPassword(body.password),
+            },
+          },
+        },
+      });
+
+      return c.json({
+        message: "New user has been registered",
+        newUser: {
+          username: newUser.username,
+        },
+      });
+    } catch (error) {
+      c.status(400);
+      return c.json({ message: "Cannot register user." });
+    }
+  }
+);
+
+app.post(
+  "/auth/login",
+  zValidator(
+    "json",
+    z.object({
+      username: z.string(),
+      password: z.string(),
+    })
+  ),
+  async (c) => {
+    const body = c.req.valid("json");
+
+    const foundUser = prisma.user.findUnique({
+      where: { username: body.username },
+    });
+
+    return c.json(foundUser);
+  }
+);
+
+app.get("/auth/me", async (c) => {
+  return c.json({ message: "User data" });
 });
 
 export default app;
