a9b6c18e675380516a22f1a2f4939c7821972ef0
M Haidar Hanif <git@mhaidarhanif.com>
2024-10-07 22:15:07 +0700

    feat: improve add to cart quantity

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -55,36 +55,43 @@ export const cartRoute = new Hono()
       const user = c.get("user");
       const body = c.req.valid("json");
 
+      // QUERY
       const existingCart = await prisma.cart.findFirst({
         where: { userId: user.id },
         orderBy: { createdAt: "desc" },
+        include: { items: true },
       });
 
       if (!existingCart) {
         return c.json({ message: "Shopping cart is unavailable" }, 404);
       }
 
-      // FIXME: check existing product item before proceeding
-      // Loop over for existing items in the Cart
-      // So we can increment that without creating a new CartItem
+      const existingItem = existingCart.items.find(
+        (item) => item.productId === body.productId
+      );
 
-      const updatedCart = await prisma.cart.update({
-        where: { id: existingCart.id },
-        data: {
-          items: {
-            create: {
-              productId: body.productId,
-              quantity: body.quantity,
-            },
+      if (existingItem) {
+        // QUERY: Increment the quantity of the existing item
+        await prisma.cartItem.update({
+          where: { id: existingItem.id },
+          data: { quantity: existingItem.quantity + body.quantity },
+        });
+      } else {
+        // QUERY: If the item does not exist, create a new cart item
+        await prisma.cartItem.create({
+          data: {
+            cartId: existingCart.id,
+            productId: body.productId,
+            quantity: body.quantity,
           },
-        },
-        select: {
-          items: {
-            orderBy: {
-              createdAt: "asc",
-            },
-          },
-        },
+        });
+      }
+
+      // QUERY
+      const updatedCart = await prisma.cart.findFirst({
+        where: { userId: user.id },
+        orderBy: { createdAt: "desc" },
+        include: { items: true },
       });
 
       return c.json(updatedCart);
